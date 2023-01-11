bce94d84c295c52ba63827b224e07f09bc145f54
Toma Adrian Ionut <sirhadrian21@gmail.com>
2023-01-12 00:39:28 +0200

    wifi toggle

--- wifi_menu.sh
+++ wifi_menu.sh
@@ -4,8 +4,14 @@ SCRIPTPATH="$(cd "$(dirname "$0")" || exit && pwd -P)"
 
 LIST="$(nmcli --fields "SSID,SECURITY,BARS" device wifi)"
 
+WIFISTATUS="$(nmcli radio wifi)"
 
-echo -e "$LIST" | rofi -dmenu -p "Wi-Fi SSID: " -matching regex -config "$SCRIPTPATH/wifi_config.rasi" 
+
+[[ "$WIFISTATUS" =~ "enabled" ]] && TOGGLE="Turn wifi off"
+[[ "$WIFISTATUS" =~ "disabled" ]] && TOGGLE="Turn wifi on"
+
+
+echo -e "$TOGGLE\n$LIST" | rofi -dmenu -p "Wi-Fi SSID: " -matching regex -config "$SCRIPTPATH/wifi_config.rasi" 
 
 
 
