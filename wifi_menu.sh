9285e1f46531e939f843f311e253cba1c703206c
Toma Adrian Ionut <sirhadrian21@gmail.com>
2023-01-12 13:50:31 +0200

    config

--- wifi_menu.sh
+++ wifi_menu.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/env bash
 
+# Config
+#==========================================================
 SCRIPTPATH="$(cd "$(dirname "$0")" || exit && pwd -P)"
+# Config for rofi-wifi-menu
+# position values:
+# 1 2 3
+# 8 0 4
+# 7 6 5
+POSITION=2
+#x-offset
+X_OFFSET=355
+#y-offset
+Y_OFFSET=50
+#fields to be displayed
+FIELDS=SSID,SECURITY,BARS
+#font
+FONT="JetBrainsMono Nerd Font 10"
+#==========================================================
 
-LIST="$(nmcli --fields "SSID,SECURITY,BARS" device wifi)"
-
-WIFISTATUS="$(nmcli radio wifi)"
-
-
-[[ "$WIFISTATUS" =~ "enabled" ]] && TOGGLE="Turn wifi off"
-[[ "$WIFISTATUS" =~ "disabled" ]] && TOGGLE="Turn wifi on"
+LIST="$(nmcli --fields "$FIELDS" device wifi)"
+WIFI_STATUS="$(nmcli radio wifi)"
 
 
-RESULT="$(echo -e "$TOGGLE\n$LIST" | rofi -dmenu -p "Wi-Fi SSID: " -matching regex -config "$SCRIPTPATH/wifi_config.rasi")"
+[[ "$WIFI_STATUS" =~ "enabled" ]] && TOGGLE="Turn wifi off"
+[[ "$WIFI_STATUS" =~ "disabled" ]] && TOGGLE="Turn wifi on"
+
+
+RESULT="$(echo -e "$TOGGLE\n$LIST" | rofi -dmenu -p "Wi-Fi SSID: " -matching regex -config "$SCRIPTPATH/wifi_config.rasi" -location "$POSITION" -yoffset "$Y_OFFSET" -xoffset "$X_OFFSET" -font "$FONT")"
 
 
 [[ "$RESULT" =~ "Turn wifi off" ]] && nmcli radio wifi off
 [[ "$RESULT" =~ "Turn wifi on" ]] && nmcli radio wifi on
 
 
-
+echo $RESULT
